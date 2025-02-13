b617494214f4ed0b0beb00a19900539192d221c3
osSQL <os.cloudsql@gmail.com>
2025-02-13 12:26:15 +0300

    0.2

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import { Container } from './container';
 import * as React from "react";
+import Image from 'next/image';
 
 interface Props {
 className?: string;
@@ -9,6 +10,13 @@ export const Header: React.FC<Props> = ({ className }) => {
     return (
         <header className={cn('border border-b', className)}>
             <Container className='flex items-center justify-between py-8'>
+                <div>
+                    <Image src='/Pizza.svg' alt='' width={30} height={30} />
+                    <h1>NEXT PIZZA</h1>
+                    <h3>вкусней уже некуда</h3>
+                </div>
+                <div></div>
+                <div></div>
             </Container>
         </header>
     );
