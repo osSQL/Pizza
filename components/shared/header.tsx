aebfc0309b1dca9fca8a030bfd5eaba667a5eed1
osSQL <os.cloudsql@gmail.com>
2025-02-20 12:25:34 +0300

    header 50%

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -10,12 +10,18 @@ export const Header: React.FC<Props> = ({ className }) => {
     return (
         <header className={cn('border border-b', className)}>
             <Container className='flex items-center justify-between py-8'>
-                <div>
-                    <Image src='/Pizza.svg' alt='' width={30} height={30} />
-                    <h1>NEXT PIZZA</h1>
-                    <h3>вкусней уже некуда</h3>
+                <div className='flex items-center'>
+                    <Image src='/pizza.svg' alt='' width={30} height={30} className="mr-4" />
+                    <div>
+                        <h1 className='font-normal hover:font-bold'>NEXT PIZZA</h1>
+                        <h3>вкусней уже некуда</h3>
+                    </div>
                 </div>
-                <div></div>
+                <div className='flex items-center border-solid border border-gray-400'>
+                    <Image src="/search.svg" alt='' width={30} height={30} className='mr-4'/>
+                    <h2>Поиск пиццы...</h2>
+
+                </div>
                 <div></div>
             </Container>
         </header>
